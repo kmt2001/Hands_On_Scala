93fc3a190092e5efe2efda8fdba1310401fe9c4e
Kmt2001 <krishna.tiwari@knoldus.com>
2023-03-07 15:18:39 +0530

    CountDivisors file added

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -6,7 +6,9 @@ object PalindromeTest extends App {
   //println(Palindrome.checkPalindrome("")) // prints YES
   //Testing for Toggle function
   //println(Toggle.changeCase("kriShnA"))
-    println(CountDivisors.countDivisibleNumbers(1,15,3))
-    println(CountDivisors.countDivisibleNumbers(10,100,5))
-    println(CountDivisors.countDivisibleNumbers(3,1700,2))
+    //println(CountDivisors.countDivisibleNumbers(1,15,3))
+    //println(CountDivisors.countDivisibleNumbers(10,100,5))
+    //println(CountDivisors.countDivisibleNumbers(3,1700,2))
+    println(FoundInteger.foundInteger("Kris32na"))
+    println(FoundInteger.foundInteger("Krishna"))
 }
