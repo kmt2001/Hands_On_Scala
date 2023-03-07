03b811bb41d1f62954290eff5d977bec3e9b6bf2
Kmt2001 <krishna.tiwari@knoldus.com>
2023-03-07 12:18:47 +0530

    CountDivisors file added

---
+++ src/main/scala/CountDivisors.scala
@@ -0,0 +1,13 @@
+object CountDivisors {
+  def countDivisibleNumbers(number1: Int, number2: Int, number3: Int): Int = {
+    var count_divisibility = 0
+    for (i <- number1 to number2) {
+      if (i % number3 == 0) {
+        count_divisibility += 1
+      }
+    }
+    return count_divisibility
+  }
+
+
+}
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,9 +1,12 @@
 object PalindromeTest extends App {
-  println(Palindrome.checkPalindrome("aba"))
-  println(Palindrome.checkPalindrome("racecar")) // prints YES
-  println(Palindrome.checkPalindrome("hello")) // prints NO
-  println(Palindrome.checkPalindrome("a man a plan a canal Panama")) // prints YES
-  println(Palindrome.checkPalindrome("")) // prints YES
-  println(Toggle.changeCase("kriShnA"))
-
+  //println(Palindrome.checkPalindrome("aba"))
+  //println(Palindrome.checkPalindrome("racecar")) // prints YES
+  //println(Palindrome.checkPalindrome("hello")) // prints NO
+  //println(Palindrome.checkPalindrome("a man a plan a canal Panama")) // prints YES
+  //println(Palindrome.checkPalindrome("")) // prints YES
+  //Testing for Toggle function
+  //println(Toggle.changeCase("kriShnA"))
+    println(CountDivisors.countDivisibleNumbers(1,15,3))
+    println(CountDivisors.countDivisibleNumbers(10,100,5))
+    println(CountDivisors.countDivisibleNumbers(3,1700,2))
 }
